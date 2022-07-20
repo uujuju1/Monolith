afb6fb02204df22a05a770e7201c7ebdbd8c773f
uujuju1 <j4224253@gmail.com>
2022-07-20 01:16:11 -0300

    discord light mode

--- src/monolith/blocks/defense/AOEBlock.java
+++ src/monolith/blocks/defense/AOEBlock.java
@@ -58,15 +58,15 @@ public class AOEBlock extends Block {
 				t.add(new Image(Core.atlas.find("monolith-icon-bullet"))).size(48f).padLeft(10f).padRight(10f).padTop(10f).padBottom(10f);
 				
 				t.table(stats -> {
-					stats.setBackground(Tex.whiteui);
+					stats.setBackground(Tex.underline);
 					stats.add(Core.bundle.get("stat.damage") + damage).row();
 					stats.add(Core.bundle.get("stat.range") + range/8 + StatUnit.blocks.localized());
 				}).padRight(48f).row();
 					
 				t.table(craft -> {
-					craft.setBackground(Tex.whiteui);
-					craft.add(Core.bundle.get("stat.productionTime") + ": " + craftTime/60f + StatUnit.seconds.localized()).row();
-					craft.add(Core.bundle.get("stat.reload") + ": " + reloadTime/60f + StatUnit.seconds.localized());
+					craft.setBackground(Tex.underline);
+					craft.add(Core.bundle.get("stat.productiontime") + ": " + craftTime/60f +  " " + StatUnit.seconds.localized()).row();
+					craft.add(Core.bundle.get("stat.reload") + ": " + reloadTime/60f +  " " + StatUnit.seconds.localized());
 				});
 			});
 		});
