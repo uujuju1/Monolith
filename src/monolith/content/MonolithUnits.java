0df7bdab75d886fe5cd6a076db10475f2808b3db
uujuju1 <j4224253@gmail.com>
2022-07-25 16:12:30 -0300

    wepoons

--- src/monolith/content/MonolithUnits.java
+++ src/monolith/content/MonolithUnits.java
@@ -2,18 +2,41 @@ package monolith.content;
 
 import mindustry.gen.*;
 import mindustry.type.*;
+import mindustry.entities.bullet.*;
 import monolith.type.*;
 
 public class MonolithUnits {
-  public static UnitType test;
+	public static UnitType test;
 
-  public void load() {
-    test = new SubmersibleUnitType("test") {{
-      health = 1650;
-      speed = 1f;
-      range = maxRange = 25f * 8f;
-      hitSize = 12f;
-      constructor = LegsUnit::create;
-    }};
-  }
+	public void load() {
+		test = new SubmersibleUnitType("test") {{
+			health = 1650;
+			speed = 1f;
+			range = maxRange = 25f * 8f;
+			hitSize = 12f;
+			constructor = MechUnit::create;
+			immunitites.add(MonolithStatusEffects.overrun);
+
+			weapons.add(
+				new Weapon("monolith-shelter-cannon") {{
+					x = 9.5f;
+					y = 0f;
+					reload = 60f;
+					recoil = 2f;
+					bullet = new BasicBuleltType(2.5f, 60) {{
+						lifetime = 80f;
+					}};
+				}},
+				new Weapon("monolith-shelter-mount") {{
+					x = 3f;
+					y = -6f;
+					reload = 30f;
+					recoil = 2f;
+					bullet = new BasicBuleltType(2.5f, 15) {{
+						lifetime = 80f;
+					}};
+				}}
+			);
+		}};
+	}
 }
--- src/monolith/type/SubmersibleUnitType.java
+++ src/monolith/type/SubmersibleUnitType.java
@@ -6,6 +6,7 @@ import mindustry.*;
 import mindustry.gen.*;
 import mindustry.game.*;
 import mindustry.type.*;
+import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.world.blocks.environment.*;
 
@@ -13,13 +14,14 @@ public class SubmersibleUnitType extends UnitType {
 	public Effect submersedEffect = new Effect(30f, e -> {
 		Draw.color(e.color);
 		Lines.stroke(e.foutpow());
-		Lines.circle(e.x, e.y, 2f * e.finpow());
+		Lines.circle(e.x, e.y, 5f * e.finpow());
 	});
 	public float submersedEffectChance = 0.04f;
 
 	public SubmersibleUnitType(String name) {
 		super(name);
 		canDrown = false;
+		immunitites.add(StatusEffects.wet);
 	}
 
 	public Floor getFloor(Unit unit) {
@@ -28,21 +30,21 @@ public class SubmersibleUnitType extends UnitType {
 
 	@Override
 	public boolean targetable(Unit unit, Team targeter) {
-		if (getFloor(unit).isLiquid) return false;
+		if (getFloor(unit).drownTime == 0) return false;
 		return targetable || (vulnerableWithPayloads && unit instanceof Payloadc p && p.hasPayload());
 	}
 
 	@Override
 	public boolean hittable(Unit unit) {
-		if (getFloor(unit).isLiquid) return false;
+		if (getFloor(unit).drownTime == 0) return false;
 		return hittable || (vulnerableWithPayloads && unit instanceof Payloadc p && p.hasPayload());
 	}
 
 	@Override
 	public void update(Unit unit) {
 		super.update(unit);
-		if(Mathf.chanceDelta(submersedEffectChance)){
-			submersedEffect.at(unit.x + Mathf.range(unit.type.hitSize * 8f), unit.y + Mathf.range(unit.type.hitSize * 8f));
+		if(Mathf.chanceDelta(submersedEffectChance) && getFloor(unit).drownTime == 0){
+			submersedEffect.at(unit.x + Mathf.range(unit.type.hitSize), unit.y + Mathf.range(unit.type.hitSizew), unit.rotation, getFloor(unit).mapColor);
 		}
 	}
 }
