c47d2d768ba8d01eec3016a2f142ea865086423d
uujuju1 <j4224253@gmail.com>
2022-07-27 22:35:36 -0300

    ...

--- src/monolith/blocks/dimension/TilesDrawable.java
+++ src/monolith/blocks/dimension/TilesDrawable.java
@@ -4,6 +4,7 @@ import arc.math.*;
 import arc.graphics.g2d.*;
 import arc.scene.style.*;
 import mindustry.world.*;
+import mindustry.content.*;
 
 public class TilesDrawable extends BaseDrawable implements TransformDrawable {
 	public Tiles tiles;
@@ -14,14 +15,13 @@ public class TilesDrawable extends BaseDrawable implements TransformDrawable {
 
 	@Override
 	public void draw(float initx, float inity, float width, float height){
-		Draw.color(Tmp.c1.set(tint).mul(Draw.getColor()).toFloatBits());
-		tiles.eachtile(tile -> {
+		tiles.eachTile(tile -> {
 			Mathf.rand.setSeed(tile.pos());
 			if (tile.floor() != null && tile.floor() != Blocks.air) {
-				Draw.rect(tile.floor().variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))], initx + tile.worldx(), initx + tile.worldy());
+				Draw.rect(tile.floor().variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, tile.floor().variantRegions.length - 1))], initx + tile.worldx(), initx + tile.worldy());
 			}
 			if (tile.overlay() != null && tile.overlay() != Blocks.air) {
-				Draw.rect(tile.overlay().variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))], initx + tile.worldx(), initx + tile.worldy());
+				Draw.rect(tile.overlay().variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, tile.overlay().variantRegions.length - 1))], initx + tile.worldx(), initx + tile.worldy());
 			}
 			if (tile.block() != null && tile.block() != Blocks.air) {
 				Draw.rect(tile.block().uiIcon, initx + tile.worldx(), initx + tile.worldy());
