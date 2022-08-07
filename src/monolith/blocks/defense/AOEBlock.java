55064de49c5d513e9250c1c768655516514e4528
uujuju1 <j4224253@gmail.com>
2022-08-07 18:03:22 -0300

    import, the errors come pouring in

--- src/monolith/blocks/defense/AOEBlock.java
+++ src/monolith/blocks/defense/AOEBlock.java
@@ -21,8 +21,8 @@ import mindustry.world.meta.*;
 import mindustry.ui.dialogs.*;
 import mindustry.world.consumers.*;
 import monolith.ui.*;
-import mindustry.type.*;
-import mindustry.type.modifiers.*;
+import monolith.type.*;
+import monolith.type.modifiers.*;
 
 public class AOEBlock extends Block {
 	public Cons<Building> drawer = build -> {Draw.rect(region, build.x, build.y, rotate ? build.rotdeg() : 0);};
