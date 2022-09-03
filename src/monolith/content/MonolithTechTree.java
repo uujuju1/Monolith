125a6e06dc3e7d1b227b6e99e3c4827d062f9b1e
uujuju1 <j4224253@gmail.com>
2022-09-03 17:25:22 -0300

    fix 6

--- src/monolith/content/MonolithTechTree.java
+++ src/monolith/content/MonolithTechTree.java
@@ -10,26 +10,26 @@ public class MonolithTechTree {
 	public void load() {
 		MonolithPlanets.chroma.techTree = nodeRoot("chroma", MonolithBlocks.furnace, true, () -> {
 			nodeProduce(MonolithItems.macrosteel, () -> {
-				nodeProduce(MonolithItems.lithium, () -> {
-					
-				});
+				nodeProduce(MonolithItems.lithium, () -> {});
 			});
 
-			node(MonolithBlocks.itemLiquidJunction, Seq.with(MonolithItems.macrosteel));
+			node(MonolithBlocks.itemLiquidJunction, Seq.with(MonolithItems.macrosteel), () -> {});
 
-			node(MonolithBlocks.lithiumWeaver, Seq.with(new Produce(MonolithItems.macrosteel)));
+			node(MonolithBlocks.lithiumWeaver, Seq.with(new Produce(MonolithItems.macrosteel)), () -> {});
 
 			node(MonolithBlocks.move, Seq.with(new Produce(MonolithItems.macrosteel)), () -> {
-				node(MonolithBlocks.accelerate, Seq.with(new Produce(Items.thorium)));
+				node(MonolithBlocks.accelerate, Seq.with(new Produce(Items.thorium)), () -> {});
+
 				node(MonolithBlocks.caesar, Seq.with(new Produce(Items.plastanium)), () -> {
-					node(MonolithBlocks.vigenere, Seq.with(new Produce(Items.thorium)));
+					node(MonolithBlocks.vigenere, Seq.with(new Produce(Items.thorium)), () -> {});
 				});
-				node(MonolithBlocks.revenant, Seq.with(new Produce(MonolithItems.lithium)));
+
+				node(MonolithBlocks.revenant, Seq.with(new Produce(MonolithItems.lithium)), () -> {});
 			});
 
-			node(MonolithBlocks.artifact, Seq.with(new Produce(Items.plastanium)));
+			node(MonolithBlocks.artifact, Seq.with(new Produce(Items.plastanium)), () -> {});
 
-			node(MonolithBlocks.sparkWall, Seq.with(new Produce(MonolithItems.lithium)));
+			node(MonolithBlocks.sparkWall, Seq.with(new Produce(MonolithItems.lithium)), () -> {});
 		});
 	}
 }
