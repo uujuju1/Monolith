1fab6bea46773aa5b539bf2fb41a040ea6cbb679
uujuju1 <j4224253@gmail.com>
2022-09-28 20:58:09 -0300

    C:/Program Files/Git/

--- src/monolith/content/MonolithLiquids.java
+++ src/monolith/content/MonolithLiquids.java
@@ -4,16 +4,16 @@ import arc.graphics.*;
 import mindustry.type.*;
 
 public class MonolithLiquids {
-	public static Liquid pitch, markyic, hydroxia, methane;
+	public static Liquid pitch, makyic, hydroxia, methane;
 
 	public void load() {
 		pitch = new Liquid("pitch", Color.valueOf("232B23")) {{
 			viscosity = 0.9f;
 			flammability = 0.3f;
 		}};
-		markyic = new Liquid("makyic", Color.valueOf("925439")) {{
+		makyic = new Liquid("makyic", Color.valueOf("925439")) {{
 			temperature = 0.2f;
-			viscosity = 0.2f/
+			viscosity = 0.2f;
 		}};
 		hydroxia = new Liquid("hydroxia", Color.valueOf("CEB2FE")) {{
 			gas = true;
