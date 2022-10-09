8f24c0632a32e010d7f1d6cb617fec0db0a68b0c
uujuju1 <j4224253@gmail.com>
2022-10-08 22:59:06 -0300

    aaa

--- src/monolith/world/blocks/distribution/PressurePipe.java
+++ src/monolith/world/blocks/distribution/PressurePipe.java
@@ -26,7 +26,9 @@ public class PressurePipe extends PressureBlock {
 	}
 
 	@Override
-	public TextureRegion[] regions() {return new TextureRegion[]{regions[0]};}
+	public TextureRegion[] icons() {
+		return new TextureRegion[]{regions[0]};
+	}
 
 	public class PressurePipeBuild extends PressureBuild {
 		public int findRegion() {
