dc5a7aab953016cca3d8af71edebdc07c646f0ab
uujuju1 <j4224253@gmail.com>
2022-10-18 13:01:16 -0300

    why

--- src/monolith/content/MonolithPlanets.java
+++ src/monolith/content/MonolithPlanets.java
@@ -112,7 +112,7 @@ public class MonolithPlanets {
 					for (Room room : rooms) {
 						while (room.connect == null || room.connect != room) room.connect = rooms.random(rand());
 						p.erase(room.x, room.y, room.r);
-						p.brush(p.pathfind(room.x, room.y, room.connect.x, room.connect.y, tile -> 999999999f, Astar.manhattan, tile -> true), rand().random(10, 20));
+						p.brush(Astar.pathfind(room.x, room.y, room.connect.x, room.connect.y, tile -> Math.dst(width()/2f, height()/2f) - tile.dst(width()/2f, height()/2f)/10f, Astar.manhattan, tile -> true), rand().random(10, 20));
 					}
 				};
 			}};
