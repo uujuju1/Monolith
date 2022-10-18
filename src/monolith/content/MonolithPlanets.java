9f5eba467f8332bb6e3e6f1127ffdcac2654f1c9
uujuju1 <j4224253@gmail.com>
2022-10-18 13:43:38 -0300

    forn't

--- src/monolith/content/MonolithPlanets.java
+++ src/monolith/content/MonolithPlanets.java
@@ -113,9 +113,9 @@ public class MonolithPlanets {
 					for (Room room : rooms) {
 						while (room.connect == null || room.connect != room) room.connect = rooms.random(rand());
 						p.erase(room.x, room.y, room.r);
+						Seq<Tile> path = Astar.pathfind(Vars.world.tiles.getn(room.x, room.y), Vars.world.tiles.getn(room.conncect.x, room.conncect.y), tile -> Mathf.dst(width()/2f, height()/2f), tile -> true);
+						for (Tile tile : path) p.erase(tile.x, tile.y, 10);
 					}
-					Seq<Tile> path = Astar.pathfind(Vars.world.tiles.getn(room.x, room.y), Vars.world.tiles.getn(room.conncect.x, room.conncect.y), tile -> Mathf.dst(width()/2f, height()/2f), tile -> true);
-					for (Tile tile : path) p.erase(tile.x, tile.y, 10);
 				};
 			}};
 			atmosphereColor = Color.valueOf("809A5E");
