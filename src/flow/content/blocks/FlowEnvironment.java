cf35560b52f08c990b6365ebb355a516b48d0ebf
uujuju1 <j4224253@gmail.com>
2022-10-27 15:01:25 -0300

    ...

--- src/flow/content/blocks/FlowEnvironment.java
+++ src/flow/content/blocks/FlowEnvironment.java
@@ -1,7 +1,7 @@
 package flow.content.blocks;
 
 import mindustry.world.*;
-import mindustry.world.blocks.*;
+import mindustry.world.meta.*;
 import mindustry.world.blocks.environment.*;
 
 public class FlowEnvironment {
@@ -15,14 +15,14 @@ public class FlowEnvironment {
 	lodeWall, acrylicWall, mosaicWall, oxaicWall;
 
 	public static void load() {
-		crystalIce = new Floor("crystal-ice") {{attributes.set(Attributes.water, 0.4f);}};
-		crystalSnow = new Floor("crystal-snow") {{attributes.set(Attributes.water, 0.3f);}};
+		crystalIce = new Floor("crystal-ice") {{attributes.set(Attribute.water, 0.4f);}};
+		crystalSnow = new Floor("crystal-snow") {{attributes.set(Attribute.water, 0.3f);}};
 		crystalStone = new Floor("crystal-stone");
 		flakeStone = new Floor("flake-stone");
 		cupricStone = new Floor("cupric-stone");
-		lodeStone = new Floor("lode-stone") {{attributes.set(Attributes.water, -0.25f);}};
+		lodeStone = new Floor("lode-stone") {{attributes.set(Attribute.water, -0.25f);}};
 		acrylicStone = new Floor("acrylic-stone");
-		mosaicStone = new Floor("mosaic-stone") {{attributes.set(Attributes.water, -0.5f);}};
+		mosaicStone = new Floor("mosaic-stone") {{attributes.set(Attribute.water, -0.5f);}};
 		oxaicStone = new Floor("oxaic-stone");
 
 		crystalIceWall = new StaticWall("crystal-ice-wall") {{crystalIce.asFloor().wall = this;}};
